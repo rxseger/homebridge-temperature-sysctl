c049375d14dbbd6d694653b6f2ff8e4678685f97
rxseger <rseger@gmx.co.uk>
2016-10-07 07:54:52 +0000

    Homebridge-ify

--- sysctl.js
+++ sysctl.js
@@ -3,19 +3,59 @@
 const sysctl = require('sysctl');
 const undotpath = require('undotpath');
 
-const keys = {
-  'hw.acpi.thermal.tz1.temperature': 'ACPI TZ1',
-  'hw.acpi.thermal.tz0.temperature': 'ACPI TZ0',
-  'dev.cpu.7.temperature': 'CPU #7',
-  'dev.cpu.6.temperature': 'CPU #6',
-  'dev.cpu.5.temperature': 'CPU #5',
-  'dev.cpu.4.temperature': 'CPU #4',
-  'dev.cpu.3.temperature': 'CPU #3',
-  'dev.cpu.2.temperature': 'CPU #2',
-  'dev.cpu.1.temperature': 'CPU #1',
-  'dev.cpu.0.temperature': 'CPU #0',
+let Service, Characteristic;
+
+module.exports = (homebridge) => {
+  Service = homebridge.hap.Service;
+  Characteristic = homebridge.hap.Characteristic;
+
+  homebridge.registerAccessory('homebridge-sysctl', 'sysctl', SysctlPlugin);
 };
 
-sysctl('dev.cpu.0.temperature')
-.then(r => console.log(undotpath(r)));
+class SysctlPlugin
+{
+ constructor(log, config) {
+    this.log = log;
 
+    // Sensors of interest, mapping sysctl name to Homebridge name
+    this.sysctls = config.sysctls || {
+      'hw.acpi.thermal.tz1.temperature': 'ACPI TZ1',
+      'hw.acpi.thermal.tz0.temperature': 'ACPI TZ0',
+      'dev.cpu.7.temperature': 'CPU #7',
+      'dev.cpu.6.temperature': 'CPU #6',
+      'dev.cpu.5.temperature': 'CPU #5',
+      'dev.cpu.4.temperature': 'CPU #4',
+      'dev.cpu.3.temperature': 'CPU #3',
+      'dev.cpu.2.temperature': 'CPU #2',
+      'dev.cpu.1.temperature': 'CPU #1',
+      'dev.cpu.0.temperature': 'CPU #0',
+    };
+
+    this.sensors = [];
+    Object.keys(this.sysctls).forEach((sysctlName) => {
+      const name = this.sysctls[sysctlName];
+      const subtype = sysctlName; // subtype must be unique per uuid
+      const tempSensor = new Service.TemperatureSensor(name, subtype);
+      tempSensor
+        .getCharacteristic(Characteristic.CurrentTemperature)
+        .on('get', this.getTemperature.bind(this, sysctlName));
+      this.sensors.push(tempSensor);
+    });
+  }
+
+  getTemperature(sysctlName, cb) {
+    sysctl(sysctlName).then(result => {
+        let value = undotpath(result);
+        value = parseFloat(value);
+        console.log(`got sysctl ${sysctlName} = ${value}`);
+
+        // degrees C
+        cb(null, value);
+    }).catch(err => cb(err));
+  }
+  // TODO: other services?
+
+  getServices() {
+    return this.sensors;
+  }
+}
